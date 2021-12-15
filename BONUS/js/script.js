befea27ae77a08713b3ac978c0825d61b3989438
AndreaNocciolini <andreanocciolini93@gmail.com>
2021-12-15 15:17:08 +0100

    start bonus

---
+++ BONUS/js/script.js
@@ -0,0 +1,16 @@
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+const title = new Vue(
+    {
+        el: `#title`,
+        data : {
+            message: `Hello world!`,
+            color: `white`
+        }
+    }
+)
+
+// Inseriamo un input per visualizzare in pagina un testo inserito da un utente.
+// Inseriamo una immagine e accanto un numero che identifica il numero di prodotti desiderati, i dati devono venire da Vue.
+// Poi aggiungiamo un pulsante per aumentare questo numero.
