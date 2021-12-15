ad2ac1c1ec2360de54a43c0a8686dc6193b615ca
AndreaNocciolini <andreanocciolini93@gmail.com>
2021-12-15 16:00:46 +0100

    add bonus

--- BONUS/js/script.js
+++ BONUS/js/script.js
@@ -7,9 +7,40 @@ const title = new Vue(
         data : {
             message: `Hello world!`,
             color: `white`
+        },
+    }
+)
+
+const text = new Vue(
+    {
+    el: `#text`,
+    data : {
+        text: ``,
+        placeHolder: `Write here` 
         }
     }
 )
+
+const img = new Vue (
+    {
+        el: "#container-img",
+        data : {
+            image: `https://upload.wikimedia.org/wikipedia/commons/thumb/1/1f/Oryctolagus_cuniculus_Rcdo.jpg/1200px-Oryctolagus_cuniculus_Rcdo.jpg`,
+            number: 1
+        },
+        methods : {
+            addOne: function() {
+                this.number += 1
+            },
+            removeOne: function() {
+                if (this.number > 1) {
+                   this.number -= 1 
+                }
+            }
+        }
+    }
+)
+
 
 // Inseriamo un input per visualizzare in pagina un testo inserito da un utente.
 // Inseriamo una immagine e accanto un numero che identifica il numero di prodotti desiderati, i dati devono venire da Vue.
